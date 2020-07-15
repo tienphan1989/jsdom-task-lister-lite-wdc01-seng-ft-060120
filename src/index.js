1386e1a208c2453866827256adeceb9eddfe0e28
Tien Phan <tienphan1989@gmail.com>
2020-07-14 22:01:27 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+const taskForm = document.getElementById('create-task-form');
+const tasks = document.getElementById('tasks');
+
+taskForm.addEventListener('submit', addToDo);
+
+function addToDo(event) {
+  event.preventDefault();
+  let newToDo = createToDo();
+  createButton();
+  assignPriority();
+  addDueDate();
+}
+
+function createToDo() {
+  newToDo = tasks.appendChild(document.createElement('li'));
+  newToDo.innerText = document.getElementById('new-task-description').value;
+  return newToDo; 
+}
+
+function createButton() {
+  button = document.createElement('button');
+  button.innerText = 'X';
+  newToDo.appendChild(button);
+  button.addEventListener("click", handleRemove );
+}
+
+function handleRemove(event){
+  event.preventDefault();
+  listItem = event.target.parentElement;
+  listItem.remove();
+}
+
+function assignPriority() {
+  priority = taskForm[1].value;
+  if (priority == "low") {
+    newToDo.style.color = 'green';
+  } else if (priority == "medium") {
+    newToDo.style.color = 'orange';
+  } else {
+    newToDo.style.color = 'red';
+  }
+}
+
+function addDueDate() {
+  dueDate = taskForm[2].value;
+  newToDo.insertAdjacentHTML(`afterbegin`, `${dueDate} - `)
+}
+
+
 });
